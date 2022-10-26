2e16730755ab9c2041ebd546afb16e2d900e9371
arthurmichaelw <arthurmichaelw@gmail.com>
2022-10-26 19:01:47 -0400

    Updated fruits app

---
+++ controllers/authController.js
@@ -0,0 +1,22 @@
+// Login
+// POST
+// Sending data to the server
+// Sending a username & password
+// Look up the user in the db --- Mongoose Method .findOne({ username: req.body.username })
+// Compare the password or credentials to the pass/cred of the foundUser
+// If they match
+// --- Authenticate the user
+
+// Signup
+// POST
+// Sending a Username & Password
+
+// Login view route
+// GET
+
+// Signup view route
+// GET
+
+// Log out
+// GET
+ 
